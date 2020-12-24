95ba04c2c90c9e2e143e2b4492fb64dca7537161
Eliju <36505898+Eliju@users.noreply.github.com>
2020-12-25 00:52:14 +0200

    Asc-desc, case insensitive-case sensitive

--- src/main/java/InsertionSort/InsertionSort.java
+++ src/main/java/InsertionSort/InsertionSort.java
@@ -1,15 +1,21 @@
 package InsertionSort;
+import java.util.HashMap;
+import java.util.function.*;
 
 public class InsertionSort {
-	public InsertionSort() {
-	}
-
-	public Integer[] sortIntegers(Integer[] intArray) {
-		Integer[] retArray = intArray.clone();
+	
+	public static <T extends Number> T[] sortNumbers(T[] intArray, String... ascOrDesc) {
+		T[] retArray = intArray.clone();
+		String aOD = "asc";
+		if (ascOrDesc.length > 0) {
+			aOD = ascOrDesc[0];
+		}
 		for (int j = 1; j < retArray.length; j++) {
-			Integer currValue = retArray[j];
+			T currValue = retArray[j];
 			int i = j - 1;
-			while ((i >= 0) && (retArray[i] > currValue)) {
+			while ((i >= 0) && 
+					((aOD.toLowerCase().equals("asc") && retArray[i].doubleValue() > currValue.doubleValue()) || 
+					((aOD.toLowerCase().equals("desc") && retArray[i].doubleValue() < currValue.doubleValue())))) {
 				retArray[i + 1] = retArray[i];
 				i--;
 			}
@@ -17,5 +23,32 @@ public class InsertionSort {
 		}
 		return retArray;
 	}
-
+	
+	public static String[] sortStrings(String[] strArray, String...params) {
+		String ascOrDesc = "asc";
+		if (params.length > 0) {
+			ascOrDesc = params[0];
+		}
+		Boolean ignoreCase = true;
+		if (params.length > 1) {
+			ignoreCase = params[1].equals("case_insensitive");
+		}
+		HashMap<String, BiFunction<String, String, Integer>> operators = new HashMap<String, BiFunction<String, String, Integer>>();
+		operators.put("case_sensitive", (x,y) -> x.compareTo(y));
+		operators.put("case_insensitive", (x,y) -> x.compareToIgnoreCase(y));
+		
+		String[] retArray = strArray.clone();
+		for (int j = 1; j < retArray.length; j++) {
+			String currValue = retArray[j];
+			int i = j - 1;
+			while ((i >= 0) && 
+					((ascOrDesc.toLowerCase().equals("asc") && ((ignoreCase ? operators.get("case_insensitive").apply(retArray[i], currValue) : operators.get("case_sensitive").apply(retArray[i], currValue)) > 0)) ||
+					((ascOrDesc.toLowerCase().equals("desc") && ((ignoreCase ? operators.get("case_insensitive").apply(retArray[i], currValue) : operators.get("case_sensitive").apply(retArray[i], currValue)) < 0))))) {
+				retArray[i + 1] = retArray[i];
+				i--;
+			}
+			retArray[i + 1] = currValue;
+		}
+		return retArray;
+	}
 }
--- src/main/java/InsertionSort/TestingSorts.java
+++ src/main/java/InsertionSort/TestingSorts.java
@@ -5,8 +5,34 @@ import java.util.Arrays;
 public class TestingSorts {
 
 	public static void main(String[] args) {
-		Integer[] sorted = new InsertionSort().sortIntegers(new Integer[]{5,2,6,2,7,0,-1,18,0,-23,-67});
-		System.out.print(Arrays.toString(sorted));
+		Integer[] intInput = new Integer[]{5,2,6,2,7,0,-1,18,0,-23,-67};
+		Double[] doubleInput = new Double[]{5.23452435,2.4674567,6.0,2.0,7.7890798,0.0,-1.578,18.1234,0.01,-23.0,-67.3456,2.4674567};
+		String[] strInput = new String[] {"aakkoset", "Aakkoset", "AlVaRi", "aLvArI", "TuOhItOrVi", "tUoHiToRvI", "Öökköset", "öökköset", "alavireinen", "ylävireinen"};
+		
+		Integer[] sortedI = InsertionSort.sortNumbers(intInput);
+		System.out.println("Integer, without params: " + Arrays.toString(sortedI));
+		sortedI = InsertionSort.sortNumbers(intInput, "asc");
+		System.out.println("Integer, with asc: " + Arrays.toString(sortedI));
+		sortedI = InsertionSort.sortNumbers(intInput, "desc");
+		System.out.println("Integer, with desc: " + Arrays.toString(sortedI));
+		System.out.println();
+		Double[] sortedD = InsertionSort.sortNumbers(doubleInput);
+		System.out.println("Double, without params: " + Arrays.toString(sortedD));
+		sortedD = InsertionSort.sortNumbers(doubleInput, "asc");
+		System.out.println("Double, with asc: " + Arrays.toString(sortedD));
+		sortedD = InsertionSort.sortNumbers(doubleInput, "desc");
+		System.out.println("Double, with desc: " + Arrays.toString(sortedD));
+		System.out.println();
+		String[] sortedS = InsertionSort.sortStrings(strInput);
+		System.out.println("String, without params (case insensitive): " + Arrays.toString(sortedS));
+		sortedS = InsertionSort.sortStrings(strInput, "asc");
+		System.out.println("String, with asc, case insensitive: " + Arrays.toString(sortedS));
+		sortedS = InsertionSort.sortStrings(strInput, "asc","case_sensitive");
+		System.out.println("String, with asc, case sensitive: " + Arrays.toString(sortedS));
+		sortedS = InsertionSort.sortStrings(strInput, "desc");
+		System.out.println("String, with desc, case insensitive: " + Arrays.toString(sortedS));
+		sortedS = InsertionSort.sortStrings(strInput, "desc","case_sensitive");
+		System.out.println("String, with desc, case sensitive: " + Arrays.toString(sortedS));
 	}
 
 }
